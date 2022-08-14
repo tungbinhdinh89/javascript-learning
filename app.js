b84a0085929449c30626b4e3dc4d91d3061a37e9
tungbinhdinh89 <tungbinhdinh89@gmail.com>
2022-08-14 15:43:46 +0700

    add multi, minus and divide

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 let button = document.getElementById('btn');
-let firsNumber = document.getElementById('firstNumber');
+let firstNumber = document.getElementById('firstNumber');
 let secondNumber = document.getElementById('secondNumber');
+let thirdNumber = document.getElementById('thirdNumber');
 let result = document.getElementById('result');
 
 // result.innerText = Number(firsNumber.value) + Number(secondNumber.value);
 
 function add(){
-    result.innerText = Number(firsNumber.value) + Number(secondNumber.value);
+    result.innerText = Number(firstNumber.value) + Number(secondNumber.value) + Number(thirdNumber.value);
     console.log('button.onclick')
 }
+function multi(){
+    result.innerText = Number(firstNumber.value) * Number(secondNumber.value) * Number(thirdNumber.value);
+}
+
+function divide(){
+    result.innerText = Number(firstNumber.value) / Number(secondNumber.value) / Number(thirdNumber.value);
+}
+
+function minus(){
+    result.innerText = Number(firstNumber.value) - Number(secondNumber.value) - Number(thirdNumber.value);
+}
+console.log('button.onclick')
