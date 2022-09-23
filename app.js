1359ce400b58b5bf8eac3221f4f0ac7e9f5e4c4b
tungbinhdinh89 <tungbinhdinh89@gmail.com>
2022-09-23 20:31:30 +0700

    sync github

--- app.js
+++ app.js
@@ -1,24 +1,46 @@
-let button = document.getElementById('btn');
-let firstNumber = document.getElementById('firstNumber');
-let secondNumber = document.getElementById('secondNumber');
-let thirdNumber = document.getElementById('thirdNumber');
-let result = document.getElementById('result');
+// let button = document.getElementById('btn');
+// let firstNumber = document.getElementById('firstNumber');
+// let secondNumber = document.getElementById('secondNumber');
+// let thirdNumber = document.getElementById('thirdNumber');
+// let result = document.getElementById('result');
 
-// result.innerText = Number(firsNumber.value) + Number(secondNumber.value);
+// // result.innerText = Number(firsNumber.value) + Number(secondNumber.value);
 
-function add(){
-    result.innerText = Number(firstNumber.value) + Number(secondNumber.value) + Number(thirdNumber.value);
-    console.log('button.onclick')
-}
-function multiply(){
-    result.innerText = Number(firstNumber.value) * Number(secondNumber.value) * Number(thirdNumber.value);
-}
+// function add(){
+//     result.innerText = Number(firstNumber.value) + Number(secondNumber.value) + Number(thirdNumber.value);
+//     console.log('button.onclick')
+// }
+// function multiply(){
+//     result.innerText = Number(firstNumber.value) * Number(secondNumber.value) * Number(thirdNumber.value);
+// }
 
-function divide(){
-    result.innerText = Number(firstNumber.value) / Number(secondNumber.value) / Number(thirdNumber.value);
-}
+// function divide(){
+//     result.innerText = Number(firstNumber.value) / Number(secondNumber.value) / Number(thirdNumber.value);
+// }
 
-function subtract(){
-    result.innerText = Number(firstNumber.value) - Number(secondNumber.value) - Number(thirdNumber.value);
-}
-console.log('button.onclick')
+// function subtract(){
+//     result.innerText = Number(firstNumber.value) - Number(secondNumber.value) - Number(thirdNumber.value);
+// }
+// console.log('button.onclick')
+
+var button = document.getElementById('addButton')
+      console.log('first time')
+
+      button.addEventListener('click', collectInformation)
+      function collectInformation() {
+        console.log('collectInformation')
+
+        var nameInput = document.getElementById('name')
+        var ageInput = document.getElementById('age')
+        var result = document.getElementById('result')
+
+        var studenHtml = `
+        <li>name: ${nameInput.value}</li>
+        <li>name: ${ageInput.value} year old</li>
+        `
+        result.innerHTML += studenHtml
+        nanmeInput.value = ''
+        ageInput.value = ''
+        nameInput.focus()
+        
+      }
